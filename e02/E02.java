b3c337a3feadb8ee36b6b745dc5b870c0772d1dc
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:30:10 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,14 @@
 class E02 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int first = sc.nextInt();
+        System.out.println("Give another number:");
+        int second = sc.nextInt();
 
+        System.out.println(String.format("%d + %d = %d", first, second, first + second));
+        System.out.println(String.format("%d * %d = %d", first, second, first * second));
+        System.out.println(String.format("%d - %d = %d", first, second, first - second));
+        System.out.println(String.format("%d / %d = %d", first, second, first / second));
     }
 }
