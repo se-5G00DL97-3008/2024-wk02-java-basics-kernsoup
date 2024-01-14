4d18e05d0313899879a1f69cf6d93ce8fa290d8f
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:52:34 +0200

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 class E06 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Which multiplication table to show?");
+        int number = sc.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(String.format("%d x %d = %d", i, number, i * number));
+        }
     }
 }
