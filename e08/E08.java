8881960a0ed5ee51058fab1a3ea974a386f85ce3
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:57:21 +0200

    Update E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Give a number (0 to quit):");
+        int number = sc.nextInt();
+        while (number != 0) {
+            if (number % 2 == 0) {
+                System.out.println("Number is even");
+            } else {
+                System.out.println("Number is odd");
+            }
+            System.out.println("Give a number (0 to quit):");
+            number = sc.nextInt();
+        }
     }
 }
