54e85c091a97de79142196742d9a37a5f45088a2
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:54:42 +0200

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class E07 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Which multiplication table to show?");
+        int number = sc.nextInt();
+        System.out.println("Until which multiplier should the table show?");
+        int other = sc.nextInt();
+        for (int i = 0; i <= other; i++) {
+            System.out.println(String.format("%d x %d = %d", i, number, i * number));
+        }
     }
 }
