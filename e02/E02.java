243aa03b5be96bac63680a0e50297f3a9665925a
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:42:43 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 class E02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Give a number:");
+        System.out.println("Give a number: ");
         int first = sc.nextInt();
-        System.out.println("Give a number:");
+        System.out.println("Give a number: ");
         int second = sc.nextInt();
 
         System.out.println(String.format("%d + %d = %d", first, second, first + second));
