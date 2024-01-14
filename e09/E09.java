6cb57759d364192655209df33aab6cd5b4f724d9
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 16:06:16 +0200

    Update E09.java

--- e09/E09.java
+++ e09/E09.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class E09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Give a number (-1 to quit):");
+        System.out.println("Give a test score (-1 to quit):");
         int number = sc.nextInt();
         float total = 0;
         int counter = 0;
@@ -11,7 +11,7 @@ class E09 {
             total += number;
             counter += 1;
             System.out.println("Average: " + total / counter);
-            System.out.println("Give a number (-1 to quit):");
+            System.out.println("Give a test score (-1 to quit):");
             number = sc.nextInt();
         }
     }
