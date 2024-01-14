7c811b295f326da3c2336319a21f2191c1441f90
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:41:55 +0200

    Update E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 class E03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number: ");
         int number = sc.nextInt();
         if (number > 0) {
             System.out.println("Positive number");
-            System.out.println("");
         } else {
             System.out.println("Negative number\n");
-            System.out.println("");
         }
     }
 }
