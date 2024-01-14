6be3b5f6474a29e367a5dc8da3544ce25d4ad405
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:44:23 +0200

    Update E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 class E04 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a day: ");
+        int day = sc.nextInt();
+        System.out.println("Enter a month: ");
+        int month = sc.nextInt();
+        if (day == 24 & month == 12) {
+            System.out.println("Merry Christmas");
+        }
     }
 }
