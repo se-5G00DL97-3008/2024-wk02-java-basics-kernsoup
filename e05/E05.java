0748115582c04c0d0cc34ac47f3df92a5ee4a3a4
kernsoup <74910541+kernsoup@users.noreply.github.com>
2024-01-14 15:49:29 +0200

    Update E05.java

--- e05/E05.java
+++ e05/E05.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 class E05 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int day = sc.nextInt();
+        switch(day) {
+            case 1:
+                System.out.println("Mon");
+                break;
+            case 2:
+                System.out.println("Tue");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thu");
+                break;
+            case 5:
+                System.out.println("Fri");
+                break;
+            case 6:
+                System.out.println("Sat");
+                break;
+            case 7:
+                System.out.println("Sun");
+                break;
+            default:
+                System.out.println("Only 7 days in a week");
+        }
     }
 }
